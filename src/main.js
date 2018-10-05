1b253827c276aaa0c38d77a78b630ca8af39c389
zerogpm <zerogpm@gmail.com>
2018-10-04 21:04:37 -0700

    setup project route

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import VueRouter from 'vue-router'
+import { routes } from "./routes";
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
