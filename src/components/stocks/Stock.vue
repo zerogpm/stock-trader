92f7b579984c1eb030ae8141706c220cb4ec4957
zerogpm <zerogpm@gmail.com>
2018-10-04 20:52:46 -0700

    creating the first components

---
+++ src/components/stocks/Stock.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Stock - stock</h1>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "Stock"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/stocks/Stocks.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Stock - Stocks</h1>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "Stocks"
+  }
+</script>
+
+<style scoped>
+
+</style>
