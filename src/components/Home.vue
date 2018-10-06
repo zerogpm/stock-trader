49988b08fd6e896580427b9fc97b8271ad5ed370
zerogpm <zerogpm@gmail.com>
2018-10-05 20:36:47 -0700

    display funds

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,20 @@
 <template>
     <div>
-        <h1>Home</h1>
+        <h1>Trade or View your Portfolio</h1>
+        <h6>You may Save And Load your Data</h6>
+        <h6>Click on 'End Day' to begin a New Day!</h6>
+        <hr>
+        <p>Your Funds: {{ funds }}</p>
     </div>
 </template>
 
 <script>
   export default {
-    name: "Home"
+    computed: {
+      funds() {
+        return this.$store.getters.funds
+      }
+    }
   }
 </script>
 
